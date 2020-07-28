d8af14f07b110f35bef75cee9e12290c64c9c5f3
Kibirige Junior Lawrence <aanga2@gmail.com>
2020-07-28 14:22:29 +0300

    Cities

--- resources/js/components/PoliceForm.js
+++ resources/js/components/PoliceForm.js
@@ -598,7 +598,7 @@ class PoliceForm extends Component {
     }
 
     getCities() {
-        axios.get('compiled_data/ugCities.json').then(response => {
+        axios.get('https://p-form.herokuapp.com/compiled_data/ugCities.json').then(response => {
             this.setState({
                 cities: response.data,
                 isLoading: false
