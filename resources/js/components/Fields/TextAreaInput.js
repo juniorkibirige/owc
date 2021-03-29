ddcf9038adbea83f98d4674462fe1fcd11c30caa
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-03-29 17:44:14 +0300

    Added TextAreaInput.js

---
+++ resources/js/components/Fields/TextAreaInput.js
@@ -0,0 +1,30 @@
+import React, {Component} from "react";
+
+class TextAreaInput extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <>
+                <div className={`form-group ` + this.props.class}>
+                    <label htmlFor={this.props.field}>{this.props.label}:<sup
+                        className='text-red'>{this.props.required ? '*' : ''}</sup></label>
+                    {
+                        <textarea
+                            className="form-control"
+                            name={this.props.field}
+                            value={this.props.value}
+                            placeholder={this.props.placeholder}
+                            required={this.props.required ?? false}
+                            onChange={this.props.onChange}
+                        />
+                    }
+                </div>
+            </>
+        );
+    }
+}
+
+export default TextAreaInput
