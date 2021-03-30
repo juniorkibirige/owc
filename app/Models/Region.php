ab00d4904d9dacc52186fb1fb303f43204f0ed82
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-03-30 21:24:49 +0300

    Added auto fill of location data in database

--- app/Models/Region.php
+++ app/Models/Region.php
@@ -6,4 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Region extends Model
 {
+    protected $table= 'regions';
+    protected $fillable = [
+        'name',
+        'slug'
+    ];
+    protected $guarded = 'id';
+    protected $primaryKey = 'id';
 }
