7e5fe1966a2132aa530dfd8d6b35508b3b64577c
Junior Lawrence Kibirige <aanga26@gmail.com>
2021-04-01 00:06:48 +0300

    Added a table

--- resources/js/components/Cruds/Supplier/List.js
+++ resources/js/components/Cruds/Supplier/List.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import axios from 'axios'
 
-import ToolkitProvider  from 'react-bootstrap-table2-toolkit'
+import ToolkitProvider from 'react-bootstrap-table2-toolkit'
 
-import { Container, Row, UncontrolledTooltip, Col, InputGroup, Input, FormGroup } from 'reactstrap'
+import {Col, Row} from 'reactstrap'
 import BootstrapTable from 'react-bootstrap-table-next'
+import TextInput from "../../Fields/TextInput";
+import DropDownInput from "../../Fields/DropDownInput";
+import Select from "react-select";
 
 const rowEvents = {
     onClick: (e, row, rowIndex) => {
@@ -17,11 +20,67 @@ class SupplierList extends Component {
         super(props)
         this.state = {
             suppliers: [],
-            newCat: '',
             alert: null
         }
         this.handleFieldChange = this.handleFieldChange.bind(this)
         document.title += " : Suppliers"
+        this.actionButtons = this.actionButtons.bind(this)
+    }
+
+    actionButtons(cell, row) {
+        let options = [
+            {
+                value: 'view',
+                label: 'View'
+            },
+            {
+                value: 'edit',
+                label: 'Edit'
+            },
+            {
+                value: 'del',
+                label: 'Delete'
+            },
+        ]
+
+        return (
+            <><Select
+                className={`basic-single `}
+                classNamePrefix={'select'}
+                isDisabled={false}
+                isLoading={false}
+                isClearable={false}
+                isRtl={false}
+                isSearchable={false}
+                name={this.props.name}
+                defaultValue={options[0]}
+                options={options}
+                // onChange={this.props.onChange}
+                // id={this.props.field}
+                isOptionDisabled={opt => opt.disabled}
+                getOptionLabel={opt => opt.label}
+                getOptionValue={opt => opt.value}
+                data-row={this.props.dataRow}
+            />
+                <Row className='mr-3 d-none'>
+                    <Col xs={4} sm={4} md={4} lg={4} xl={4}>
+                        <button className='btn btn-sm bg-green' onClick={()=>alert('View')
+                        }>View
+                        </button>
+                    </Col>
+                    <Col xs={4} sm={4} md={4} lg={4} xl={4}>
+                        <button className='btn btn-sm bg-warning'
+                                onClick={()=>alert('Edit')}>Edit
+                        </button>
+                    </Col>
+                    <Col xs={4} sm={4} md={4} lg={4} xl={4}>
+                        <button className='btn btn-sm bg-warning'
+                                onClick={()=>alert('Delete')}>Delete
+                        </button>
+                    </Col>
+                </Row>
+            </>
+        )
     }
 
     handleFieldChange() {
@@ -32,9 +91,9 @@ class SupplierList extends Component {
     }
 
     componentDidMount() {
-        axios.get('/api/supplier').then(response=>{
+        axios.get('/api/supplier').then(response => {
             this.setState({
-                suppliers: response.data,
+                suppliers: response.data.suppliers,
             });
         });
     }
@@ -44,16 +103,85 @@ class SupplierList extends Component {
         return (
             <>
                 {this.state.alert}
-                <div className='header bg-secondary pb-7'>
-                    <div className='container-fluid'>
-                        <div className='header-body'>
-                            <Row className='align-items-center py-4'>
-                                <Col>
-                                    In Development
-                                </Col>
-                            </Row>
-                        </div>
+                <div className='header' style={{minHeight: `100%`}}>
+                    <div className='container-fluid pt-4 pb-2'>
+                        <span className="text-2xl text-capitalize">Suppliers</span>
                     </div>
+                </div>
+                <div className="col-md-12 bg-blue-gray-400 pb-7">
+                    <Row className='align-items-center py-4'>
+                        <Col>
+                            <ToolkitProvider
+                                data={this.state.suppliers}
+                                keyField="id"
+                                columns={[
+                                    {
+                                        dataField: 'id',
+                                        text: 'ID',
+                                        hidden: false,
+                                        classes: 'col-md-auto'
+                                    },
+                                    {
+                                        dataField: 'name',
+                                        text: 'Name',
+                                        hidden: false,
+                                        classes: 'col-md-auto'
+                                    },
+                                    {
+                                        dataField: 'region',
+                                        text: 'Region',
+                                        hidden: false,
+                                        classes: 'col-md-auto'
+                                    },
+                                    {
+                                        dataField: 'district',
+                                        text: 'District',
+                                        hidden: false,
+                                        classes: 'col-md-auto'
+                                    },
+                                    {
+                                        dataField: 'county',
+                                        text: 'County',
+                                        hidden: false,
+                                        classes: 'col-md-auto'
+                                    },
+                                    {
+                                        dataField: 'parish',
+                                        text: 'Parish',
+                                        hidden: false,
+                                        classes: 'col-md-auto'
+                                    },
+                                    {
+                                        dataField: 'actions',
+                                        isDummyField: true,
+                                        headerStyle:()=>{
+                                            return {width: "15%"};
+                                        },
+                                        text: '',
+                                        hidden: false,
+                                        formatter: this.actionButtons
+                                    },
+                                ]}
+                                hover
+                            >
+                                {
+                                    props => (
+                                        <>
+                                            <BootstrapTable
+                                                ref={el => (this.componentRef = el)}
+                                                {...props.baseProps}
+                                                classes={`table-white`}
+                                                bootstrap4={true}
+                                                bordered={true}
+                                                noDataIndication={"No Suppliers"}
+                                                id="react-supplier-table"
+                                            />
+                                        </>
+                                    )
+                                }
+                            </ToolkitProvider>
+                        </Col>
+                    </Row>
                 </div>
             </>
         )
